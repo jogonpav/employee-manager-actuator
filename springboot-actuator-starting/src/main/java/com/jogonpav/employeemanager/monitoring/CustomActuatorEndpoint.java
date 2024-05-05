cace246ad392bfbdcce95deb2ebd3da278be6d48
unknown <josepabonvera@gmail.com>
2024-05-05 17:17:32 -0500

    Adding stater actuator project

---
+++ springboot-actuator-starting/src/main/java/com/jogonpav/employeemanager/monitoring/CustomActuatorEndpoint.java
@@ -0,0 +1,21 @@
+package com.jogonpav.employeemanager.monitoring;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Endpoint(id = "custom")
+@Component
+public class CustomActuatorEndpoint {
+
+    @ReadOperation
+    public Map<String, String> customEndpoint (String username){
+        Map<String, String> map = new HashMap<>();
+        map.put("key","value");
+        map.put("username",username);
+        return map;
+    }
+}
